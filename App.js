f07c6c902d03bc8db172c83c46035e3fbd925d94
ryeong <ryeong@ingris.com>
2025-05-15 17:54:36 +0900

    설명 추가

--- App.js
+++ App.js
@@ -4,24 +4,36 @@ import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-
+/*
     <WebView style={styles.container} source={{ uri: 'http://172.21.1.25:2735/' }}>
          <Text>Hello! i made a RN APP!!!</Text>
     </WebView>
-
-/*    <View style={styles.container}>
-      <Text>Hello! i made a RN APP!!!</Text>
-      <StatusBar style="auto" />
-    </View>*/
+*/
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello! i made a RN APP!!!</Text>
+      <StatusBar style="dark" />
+      {/* statusBar 는  시계, 배터리, wifi 를 의미함. 상태바와 소통하는것임.*/ }
+    </View>
   );
 }
 
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ /* create 는 object 를 만드는데 사용함. */ 
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize : 13,
+    color: 'black'
+  }
 });
+
+const styleTwo =  { /* 자동 완성 지원하지 않음  */
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+};

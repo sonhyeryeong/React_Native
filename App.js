19b4dfc25ba0990c12398ac099681eb6102e6d81
ryeong <ryeong@ingris.com>
2025-05-15 16:48:33 +0900

    init

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <WebView style={styles.container} source={{ uri: 'http://172.21.1.25:2735/' }}>
+         <Text>Hello! i made a RN APP!!!</Text>
+    </WebView>
+
+/*    <View style={styles.container}>
+      <Text>Hello! i made a RN APP!!!</Text>
       <StatusBar style="auto" />
-    </View>
+    </View>*/
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
